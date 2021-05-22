f04c3ac67c2ef46fc5a53040d9449285dd53de87
Michael Albert <albertmichael746@gmail.com>
2021-05-22 00:00:52 -0700

    Updated to create stack in distinct s3 bucket.

--- backend/commands.sh
+++ backend/commands.sh
@@ -1,11 +1,11 @@
 # create s3 bucket
-aws s3 mb s3://cloud-resume-bucket
+aws s3 mb s3://cloud-portfolio-bucket
 
 # package cloudformation
-aws cloudformation package --s3-bucket cloud-resume-bucket \
+aws cloudformation package --s3-bucket cloud-portfolio-bucket \
 --template-file template.yaml --output-template-file gen/template-gen.yaml
 
 # deploy
-aws cloudformation deploy --template-file gen/template-gen.yaml --stack-name cloud-formation-resume \
+aws cloudformation deploy --template-file gen/template-gen.yaml --stack-name cloud-formation-portfolio \
 --capabilities CAPABILITY_IAM
 
