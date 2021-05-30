81a9ca87103c72dfe31cd5f4c38bb04dd3d0111c
Michael Albert <albertmichael746@gmail.com>
2021-05-30 14:45:40 -0700

    Renders all components together.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Sidebar from './components/sidebar'
+import About from './components/about'
+import Education from './components/education'
+import Experience from './components/experience'
+import Home from './components/home'
+import Services from './components/services'
+import Skills from './components/skills'
+import Work from './components/work'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="cololib-page">
+        <div id="container-wrap">
+        <Sidebar></Sidebar>
+        <div id="colorlib-main">
+          <Home></Home>
+          <About></About>
+          <Education></Education>
+          <Experience></Experience>
+          <Services></Services>
+          <Skills></Skills>
+          <Work></Work>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
