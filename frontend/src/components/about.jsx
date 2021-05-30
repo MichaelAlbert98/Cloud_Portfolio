30c23c8a59d1c73b9b309fcb226c12fa6f713178
Michael Albert <albertmichael746@gmail.com>
2021-05-30 14:45:58 -0700

    Initial commit of template files.

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,48 +1,65 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
-            <div className="flexslider js-fullheight">
-                <ul className="slides">
-                <li style={{backgroundImage: 'url(images/img_bg_1.jpg)'}}>
-                    <div className="overlay" />
-                    <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                        <div className="slider-text-inner js-fullheight">
-                            <div className="desc">
-                            <h1>Hi! <br />I'm Jackson</h1>
-                            <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
-                            <p><a className="btn btn-primary btn-learn">Download CV <em className="icon-download4" /></a></p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </li>
-                <li style={{backgroundImage: 'url(images/img_bg_2.jpg)'}}>
-                    <div className="overlay" />
-                    <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                        <div className="slider-text-inner">
-                            <div className="desc">
-                            <h1>I am <br />a Designer</h1>
-                            <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
-                            <p><a className="btn btn-primary btn-learn">View Portfolio <em className="icon-briefcase3" /></a></p>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </li>
-                </ul>
-            </div>
-        </section>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<section className="colorlib-about" data-section="about">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-12">
+								<div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+									<div className="col-md-12">
+										<div className="about-desc">
+											<span className="heading-meta">About Us</span>
+											<h2 className="colorlib-heading">Who Am I?</h2>
+											<p>
+												<strong>Hi I'm Jackson Ford</strong>
+												On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country.</p>
+											<p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+										</div>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-md-3 animate-box" data-animate-effect="fadeInLeft">
+										<div className="services color-1">
+											<span className="icon2"><i className="icon-bulb"/></span>
+											<h3>Graphic Design</h3>
+										</div>
+									</div>
+									<div className="col-md-3 animate-box" data-animate-effect="fadeInRight">
+										<div className="services color-2">
+											<span className="icon2"><i className="icon-globe-outline"/></span>
+											<h3>Web Design</h3>
+										</div>
+									</div>
+									<div className="col-md-3 animate-box" data-animate-effect="fadeInTop">
+										<div className="services color-3">
+											<span className="icon2"><i className="icon-data"/></span>
+											<h3>Software</h3>
+										</div>
+									</div>
+									<div className="col-md-3 animate-box" data-animate-effect="fadeInBottom">
+										<div className="services color-4">
+											<span className="icon2"><i className="icon-phone3"/></span>
+											<h3>Application</h3>
+										</div>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-md-12 animate-box" data-animate-effect="fadeInLeft">
+										<div className="hire">
+											<h2>I am happy to know you
+												<br/>that 300+ projects done sucessfully!</h2>
+											<a href="#" className="btn-hire">Hire me</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+		)
+	}
 }
